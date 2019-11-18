c9d723e587a2cab5ffdb31b6cf73af7aaeaa6729
yx <ciilrain@126.com>
2019-11-18 11:31:21 +0800

    1
    12

--- os_check.py
+++ os_check.py
@@ -7,12 +7,16 @@ def ck(subj):
 def vck(subj):
     value = subprocess.Popen(subj, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return value.stdout.read().strip()
+subj = {
+        'ipforward':'/proc/sys/net/ipv4/ip_forward',
+        'syncookies':'/proc/sys/net/ipv4/tcp_syncookies',
+        'syn_recv_no':'/proc/sys/net/ipv4/tcp_max_syn_backlog',
+        'socketlistenbacklog':'/proc/sys/net/core/somaxconn',
+        'swap':'/proc/sys/vm/swappiness'
+        }
 
-proc = '/proc/sys/'
-subj = '/proc/sys/net/ipv4/ip_forward'
-subj2 = r'vm/swapness'
+cmdsubj = {
 
-str1 = proc + subj2
-print(str1)
-print(end2)
-exit
+}
+
+print(subj['syn_recv_no'])
